28170631bda5568eb4895103797ef232c3155d86
Tyler <tyler.bradley14@utexas.edu>
2017-03-31 14:23:48 -0500

    Added ConnectionString and AppDbContext, also EF and Bootstrap

---
+++ MIS333KProject/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MIS333KProject.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MIS333KProjectDB") { } 
+    }
+}
